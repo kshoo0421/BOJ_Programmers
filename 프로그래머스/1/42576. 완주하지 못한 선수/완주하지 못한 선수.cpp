4f3521cb9aed076717d1793c50bc5a8c7f3c30ad
kshoo0421 <95275026+kshoo0421@users.noreply.github.com>
2024-09-15 14:25:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 71.70 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -2,18 +2,11 @@
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    unordered_map<string, int> us;
-    for(auto& s : completion) {
-        if(us.find(s) == us.end()) us[s] = 0;
-        else us[s]++;
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    for(int i = 0; i < completion.size(); i++) {
+        if(participant[i] != completion[i]) 
+            return participant[i];
     }
-    for(auto& s : participant) {
-        if(us.find(s) == us.end()) return s;
-        else {
-            if(us[s] < 0) return s;
-            else us[s]--;
-        }
-        
-    }
-    return "error";
+    return participant.back();
 }
