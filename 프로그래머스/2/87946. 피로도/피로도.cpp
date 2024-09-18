9d485d5332c69aed8db582f9334d64499a82b88a
kshoo0421 <95275026+kshoo0421@users.noreply.github.com>
2024-09-18 15:32:42 +0900

    [level 2] Title: 피로도, Time: 1.16 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int solution(int k, vector<vector<int>> dungeons) {
-    int size = dungeons.size(), answer = 0;
-    vector<vector<int>> dp(size + 1, vector<int>(size + 1, 0));
-    sort(dungeons.begin(), dungeons.end(), [](vector<int>& v1, vector<int>& v2) {
-        int gap1 = v1[0] - v1[1];
-        int gap2 = v2[0] - v2[1];
-        if(gap1 != gap2) return gap1 > gap2;
-        return v1[0] > v2[0];
-    });
+    int answer = 0;
+    vector<bool> visited(dungeons.size(), false);
     
-    for(int i = 0; i <= size; i++) dp[0][i] = k;
-    
-    for(int i = 1; i <= size; i++) {
-        for(int j = 1; j <= size; j++) {
-            for(int l = i-1; l < j; l++) {
-                if(dp[i-1][l] >= dungeons[j-1][0]) {
-                    dp[i][j] = max(dp[i][j], dp[i-1][l] - dungeons[j-1][1]);
-                    answer = i;
-                }
+    function<void(int, int)> dfs = [&](int k, int count) -> void {
+        answer = max(answer, count);
+        for (int i = 0; i < dungeons.size(); ++i) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], count + 1);  
+                visited[i] = false;  
             }
-        }
-    }
+        }    
+    };
+    dfs(k, 0);
     return answer;
 }
